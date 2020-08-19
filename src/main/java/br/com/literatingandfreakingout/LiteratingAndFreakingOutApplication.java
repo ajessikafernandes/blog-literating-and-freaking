6fb67d758b8fd62546528103455dbf1ce03e9d7d
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-08-18 21:55:42 -0300

    projects#card-43903069: criando estrutura do projeto em java

---
+++ src/main/java/br/com/literatingandfreakingout/LiteratingAndFreakingOutApplication.java
@@ -0,0 +1,13 @@
+package br.com.literatingandfreakingout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteratingAndFreakingOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteratingAndFreakingOutApplication.class, args);
+	}
+
+}
